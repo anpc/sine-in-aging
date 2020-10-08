e2f4d6539e875602883043aeb83cf6f87c35b879
Ubuntu <ubuntu@ip-172-31-28-58.eu-central-1.compute.internal>
2020-10-08 15:31:31 +0000

    new script to run barcod with dict and create a graph

---
+++ graphPartScript.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.6
+
+from organized_all_2 import SINES_graph_of_neighbors
+import sys
+
+if len(sys.argv[1:]) == 3:
+	[dict_fname, barcode_name ,part] = sys.argv[1:]
+	SINES_graph_of_neighbors(dict_fname, barcode_name, int(part))
+
